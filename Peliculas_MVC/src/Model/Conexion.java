8dd8e832a4cc5e695093ae6477192e32e73b44da
viklercharles <charlesvikler@gmail.com>
2018-11-14 19:27:42 -0500

    Creando clases en el model package

--- Peliculas_MVC/src/Model/Conexion.java
+++ Peliculas_MVC/src/Model/Conexion.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package peliculas_mvc;
+package Model;
 
 /**
  *
  * @author viklercharles
  */
-public class Peliculas_MVC {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Conexion {
     
 }
